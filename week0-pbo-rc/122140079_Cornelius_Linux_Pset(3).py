c66a6b958ef143cffdaa15bff653bbe95e4b0a2b
Cornelius Linux <radi.adi78@gmail.com>
2024-02-23 01:07:16 +0700

    refactor: filename and output directory

--- week0-pbo-rc/122140079_Cornelius_Linux_Pset(3).py
+++ week0-pbo-rc/122140079_Cornelius_Linux_Pset(3).py
@@ -2,5 +2,5 @@ content = '''Nama : Cornelius Linux
 NIM : 122140079
 Resolusi Saya di Tahun ini : Create a network and simple business😋'''
 
-with open("Me.text","w",encoding="utf-8") as file:
+with open("Me.txt","w",encoding="utf-8") as file:
     file.write(content)
